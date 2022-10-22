296094f771ff36f6e7f2de9cdcc62e433743f62c
Rob Fabbro <robfabbro50@gmail.com>
2022-10-22 16:14:01 +1100

    added user guess as a state on gamepage

--- client/src/GamePage.js
+++ client/src/GamePage.js
@@ -1,50 +1,62 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
-const inputEntered = (event) => {
-  if (event.keyCode === 13) {
-    console.log("enter");
-  }
+const backToPlayPage = () => {
+  window.location = "/flags";
 };
 
-const backToPlayPage = () => {
-    window.location = "/flags";
-}
-
 const convertSecondsToTimer = (secs) => {
-    let minutes = Math.floor(secs/60);
-    let seconds = secs - (minutes * 60);
-    if(minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    if(seconds < 10){
-        seconds = `0${seconds}`;
-    }
-    return `${minutes}:${seconds}`;
-}
+  let minutes = Math.floor(secs / 60);
+  let seconds = secs - minutes * 60;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+};
 
 const GamePage = (props) => {
-    const [timer, setTimer] = useState(-1);
+  const [timer, setTimer] = useState(0);
+  const [guess, setGuess] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     //every seconds, add 1 to the timer
     setTimeout(() => {
-        setTimer((timer) => timer + 1);
+      setTimer((timer) => timer + 1);
     }, 1000);
-});
+  }, [timer]);
 
+  const handleInputChange = (event) => {
+    setGuess(event.target.value.slice(0, 56));
+  };
+
+  const inputEntered = (event) => {
+    //if entered
+    if (event.keyCode === 13) {
+      console.log(event);
+    }
+  };
 
   return (
     <div className="gamePage">
       <h1>Flags</h1>
       <p className="timer">{convertSecondsToTimer(timer)}</p>
-      <p className="giveUp" onClick={backToPlayPage}>Give Up?</p>
+      <p className="giveUp" onClick={backToPlayPage}>
+        Give Up?
+      </p>
       <span className="fi fi-mw flag"></span>
       <p className="answerReveal"></p>
       <div className="guessField">
-        <input className="guessInput" onKeyUp={inputEntered}></input>
+        <input
+          className="guessInput"
+          onChange={handleInputChange}
+          onKeyUp={inputEntered}
+          value={guess}
+        ></input>
         <div className="guessResult">
-            <i className="fa-solid fa-check"></i>
+          <i className="fa-solid fa-check"></i>
         </div>
       </div>
     </div>
