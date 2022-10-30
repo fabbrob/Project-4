2b14f11c164f9756d4e567cf268623e4595c4f52
Rob Fabbro <robfabbro50@gmail.com>
2022-10-30 15:54:15 +1100

    hi

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3001; // Note: using a different port to normal
 
-app.use(express.static("./client/build"));
 app.use(express.json());
+app.use(express.static("./client/build"));
 
 app.get("/api/test", (req, res) => {
   res.json({ result: "success" });
